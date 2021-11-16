f5fb58015106e45d3639932fd148e4724ee09ad2
MohamedElmdary <engm5081@gmail.com>
2021-11-16 11:31:00 +0200

    Added nice looking editor

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,87 +1,122 @@
 <template>
-  <div>
-    <div class="editor">
-      <div class="editor__input">
-        <textarea v-model="md"></textarea>
-      </div>
-      <div class="editor__preview" v-html="html"></div>
+  <section class="editor">
+    <aside class="editor__side menu">
+      <p class="menu-label">Threefold Weblets</p>
+      <ul class="menu-list">
+        <li v-for="(el, idx) in weblets" :key="el.name" @click="active = idx">
+          <a v-bind:class="{ 'is-active': idx === active }">
+            {{ el.name }}
+          </a>
+        </li>
+      </ul>
+    </aside>
+
+    <div class="editor__content">
+      <div v-html="weblets[active].html" />
     </div>
-  </div>
+    <div class="editor__area">
+      <textarea spellcheck="false" v-model="weblets[active].md" />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import { parse } from "marked";
+
+class Weblet {
+  private _md: string;
+  public html: string;
+  public constructor(public name: string, symbol: string) {
+    this._md = `<tf-${symbol}></tf-${symbol}>`;
+    this.html = parse(this._md, {
+      sanitize: false,
+    });
+  }
+
+  public get md(): string {
+    return this._md;
+  }
+
+  public set md(_md: string) {
+    this._md = _md;
+    this.html = parse(_md, {
+      sanitize: false,
+    });
+  }
+}
 
 @Component({
   name: "Editor",
 })
 export default class Editor extends Vue {
-  md = `
-# h1
-## h2
-### h3
-#### h4
-##### h5
-###### h6
----
-
-<tf-farming-calculator></tf-farming-calculator>
----
-<tf-kubernetes></tf-kubernetes>
-
-<tf-vm></tf-vm>
-
-<tf-caprover></tf-caprover>
-
-<tf-deployedlist></tf-deployedlist>
-
-<tf-deployedlist tab="k8s"></tf-deployedlist>
-  
-  `;
-  html = "";
-
-  @Watch("md", { immediate: true, deep: true })
-  onMdChange() {
-    this.html = parse(this.md, {
-      sanitize: false,
-    });
-  }
+  public weblets: Weblet[] = [
+    new Weblet("Kubernetes", `kubernetes`),
+    new Weblet("Virtual Machine", "vm"),
+    new Weblet("Deployed List", "deployedlist"),
+    new Weblet("CapRover", "caprover"),
+    new Weblet("Farming Calculator", "farming-calculator"),
+  ];
+  public active = 0;
 }
 </script>
 
 <style lang="scss" scoped>
-.editor {
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  display: flex;
-
-  &__input {
-    width: 20%;
-    height: 100%;
-    border: 1px solid #ccc;
-
-    textarea {
-      padding: 15px;
-      width: 100%;
-      height: 100%;
-      display: block;
-      background: #333;
-      color: white;
-      font-size: 20px;
-      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-        Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    }
+@mixin nice-scroll {
+  &::-webkit-scrollbar {
+    width: 10px;
   }
 
-  &__preview {
-    width: 80%;
-    height: 100%;
+  &::-webkit-scrollbar-thumb {
+    background-color: #d3d3d3;
+  }
+}
+
+.editor {
+  padding-left: 260px;
+  position: relative;
+
+  &__side {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 260px;
+    padding: 15px;
+    border-right: 1px solid #dbdbdb;
     overflow-x: hidden;
     overflow-y: auto;
+    @include nice-scroll;
+  }
+
+  &__content {
+    height: 100vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    will-change: transform;
+    padding-bottom: 250px;
+    @include nice-scroll;
+  }
+
+  &__area {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    width: calc(100% - 260px);
+    height: 200px;
+
+    textarea {
+      height: 100%;
+      width: 100%;
+      resize: none;
+      border: none;
+      padding: 15px;
+      background-color: #333;
+      color: #ccc;
+      font-size: 18px;
+      outline: none;
+      @include nice-scroll;
+    }
   }
 }
 </style>
